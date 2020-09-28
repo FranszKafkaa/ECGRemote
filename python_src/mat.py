4c2c2099e28bf7304ae6e923920449209359d847
Marcelo Janke <marcelosjanke@icloud.com>
2020-09-28 20:53:39 -0300

    adicionado novos arquivos do dataset http://dx.doi.org/10.17632/7dybx7wyfn.3 e convertido para .txt

--- python_src/mat.py
+++ python_src/mat.py
@@ -1,11 +1,23 @@
-import scipy.io
+import os
+from sys import argv
+from scipy.io import loadmat
 
-data = scipy.io.loadmat("python_src/Files/100m (0).mat")
-print(data["val"][0])
+path = argv[1]
 
-with open("python_src/Files/mat.txt", "w") as file:
-    file.write("# Simple Text Format\n# Sampling Rate (Hz):= 1000.00\n# Resolution:= 12\n# Labels:= ECG\n")
-    for i in data["val"][0]:
-        file.write(str(float(i)) + "\n")
+dir = os.listdir(path)
+dir = dir[1:]
 
-print("done")
+for i in dir:
+    os.mkdir("python_src/Files/" + i)
+
+    for current in os.listdir(path + "/" + i):
+
+        data = loadmat(path + "/" + i + "/" + current)
+
+        with open("python_src/Files/" + i + "/" + current[0: -4] + ".txt", "w") as file:
+            file.write("# Simple Text Format\n# Sampling Rate (Hz):= 360.00\n# Resolution:= 12\n# Labels:= ECG\n")
+
+            for num in data["val"][0]:
+                file.write(str(float(num)) + "\n")
+
+            print("python_src/Files/" + i + "/" + current[0:4] + ".txt Done")
